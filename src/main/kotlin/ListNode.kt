15651d68e7973ef4cca15437f2f58d17587b25c6
Anton Indrawan <anton.indrawan@gmail.com>
2022-08-27 13:39:44 +0200

    utility: print nodes

--- src/main/kotlin/ListNode.kt
+++ src/main/kotlin/ListNode.kt
@@ -1,3 +1,13 @@
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+
+    fun printNextNodes() {
+        print(`val`)
+        var node: ListNode? = next
+        while (node != null) {
+            print(" ${node!!.`val`}")
+            node = node!!.next
+        }
+        println()
+    }
 }
